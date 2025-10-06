4c7a9b214721f83903606b7740b9eb201788244b
Cristina Junca (Sirbu) <sirbucristina.is@gmail.com>
2025-10-06 13:34:51 +0200

    Initial commit

---
+++ src/main/java/com/cristinaj/loganalyzer/LoganalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.cristinaj.loganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoganalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoganalyzerApplication.class, args);
+	}
+
+}
