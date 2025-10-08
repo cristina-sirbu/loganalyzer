2c48bf80a33db62a8769faef5b2d84f2269a88de
Cristina Junca (Sirbu) <sirbucristina.is@gmail.com>
2025-10-08 11:30:52 +0200

    Added Analysis controller and service

--- src/main/java/com/cristinaj/loganalyzer/domain/LogEntryRepository.java
+++ src/main/java/com/cristinaj/loganalyzer/domain/LogEntryRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+
 public interface LogEntryRepository extends JpaRepository<LogEntry, Long> {
     Page<LogEntry> findByLevel(LogLevel level, Pageable pageable);
+    Page<LogEntry> findByTsUtcBetween(Instant from, Instant to, Pageable pageable);
+    Page<LogEntry> findByLevelAndTsUtcBetween(LogLevel level, Instant from, Instant to, Pageable pageable);
 }
