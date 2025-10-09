91b18697557c9cca6302c6266c4a99cf09077b36
Cristina Junca (Sirbu) <sirbucristina.is@gmail.com>
2025-10-09 08:11:53 +0200

    Added SwaggerUI

--- src/main/java/com/cristinaj/loganalyzer/LoganalyzerApplication.java
+++ src/main/java/com/cristinaj/loganalyzer/LoganalyzerApplication.java
@@ -1,8 +1,17 @@
 package com.cristinaj.loganalyzer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Log Analyzer API",
+                version = "1.0",
+                description = "Endpoints for log ingestion and analysis"
+        )
+)
 @SpringBootApplication
 public class LoganalyzerApplication {
 
